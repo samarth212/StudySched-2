ef92ed823f5fc3e487e135484419364a3aaec8b0
25sreddy <sahilreddy092007@gmail.com>
2024-07-12 15:30:58 -0400

    fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import pkg from 'ical.js';
+const { parse, Component, Event } = pkg;
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function useRegex(input) {
+const regex = /http:\/\/\S+$/;
+const match = input.match(regex);
+
+if (match) {
+
+ return match[0]
+} else {
+  console.log("No link found");
+}
 }
 
-export default App
+async function fetchCalendar(url) {
+    // Replace 'webcal://' with 'http://'
+    const httpUrl = url.replace('webcal://', 'http://');
+
+    const response = await fetch(httpUrl);
+    const data = await response.text();
+    const jcalData = parse(data);
+    const comp = new Component(jcalData);
+    const events = comp.getAllSubcomponents('vevent');
+
+    for (let i = 0; i < events.length; i++) {
+      const event = new Event(events[i]);
+      console.log(event)
+        console.log('Event:', event.summary);
+        console.log('Desc:', event.description);
+        console.log('Link:', useRegex(event.description));
+        console.log('Starts:', event.startDate.toString());
+        console.log('Ends:', event.endDate.toString());
+    }
+}
+
+fetchCalendar('webcal://schoology.dasd.org/calendar/feed/ical/1633885110/78bda1ae32ac0554bcd8d8d0aaddd5e1/ical.ics');
+
+  return (
+    <div>
+      hi
+
+  </div>
+)
+}
+ 
+
+export default App;
