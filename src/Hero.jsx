5c0fc66b26bd7b67afb1ffa54c6d6166da96ac50
Samarth <samarth212@gmail.com>
2024-07-12 16:14:31 -0400

    hero layout

---
+++ src/Hero.jsx
@@ -0,0 +1,26 @@
+import "./hero.css"
+
+const Hero = () =>{
+    return(
+
+        <>
+            <div className="flex-row">
+
+                <div className="flex-col">
+                    <p className="title">StudySched - Manage Your Time Better</p>
+                    <p className="desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. In, asperiores harum alias nostrum quaerat veritatis et ut rerum voluptatibus, quod, maiores hic molestias expedita amet ratione dolorem fugit molestiae nesciunt.</p>
+                    <p>SIGN IN HERE</p>
+                </div>
+
+                <div>
+                    <p>IMAGE HERE</p>
+                </div>
+
+            </div>
+            
+        </>
+        
+    );
+};
+
+export default Hero
