9e15480c7098b39977fddddfe82f05869943753d
25sreddy <sahilreddy092007@gmail.com>
2024-07-14 13:07:49 -0400

    removed fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,9 @@
-import pkg from 'ical.js';
-import Hero from './Hero';
+import pkg from "ical.js";
+import Hero from "./Hero";
 
 const { parse, Component, Event } = pkg;
 function App() {
-
-function useRegex(input) {
-const regex = /http:\/\/\S+$/;
-const match = input.match(regex);
-
-if (match) {
-
- return match[0]
-} else {
-  console.log("No link found");
+  return <Hero></Hero>;
 }
-}
-
-async function fetchCalendar(url) {
-    // Replace 'webcal://' with 'http://'
-    const httpUrl = url.replace('webcal://', 'http://');
-
-    const response = await fetch(httpUrl);
-    const data = await response.text();
-    const jcalData = parse(data);
-    const comp = new Component(jcalData);
-    const events = comp.getAllSubcomponents('vevent');
-
-    for (let i = 0; i < events.length; i++) {
-      const event = new Event(events[i]);
-     
-        console.log('Event:', event.summary);
-        console.log('Desc:', event.description);
-        console.log('Link:', useRegex(event.description));
-        console.log('Assignment:', useRegex(event.description).includes("assignment"));
-        console.log('Starts:', event.startDate.toString());
-        console.log('Ends:', event.endDate.toString());
-    }
-}
-
-fetchCalendar('webcal://schoology.dasd.org/calendar/feed/ical/1720649393/8ce31bbb9b0e97d56b45830bddc12635/ical.ics');
-
-  return (
-
-      <Hero></Hero>
-
-
-)
-}
- 
 
 export default App;
