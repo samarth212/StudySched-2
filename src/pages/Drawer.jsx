1e67861d3e0abc90cad06ab16f7f9455bba46d2c
Samarth <samarth212@gmail.com>
2024-07-15 11:21:07 -0400

    sidebar styling

--- src/pages/Drawer.jsx
+++ src/pages/Drawer.jsx
@@ -14,6 +14,7 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
+import { hexToRgb } from "@mui/material";
 
 const drawerWidth = 240;
 
@@ -28,6 +29,8 @@ export default function SideDrawer() {
           "& .MuiDrawer-paper": {
             width: drawerWidth,
             boxSizing: "border-box",
+            backgroundColor: "#111111",
+            color: "white"
           },
         }}
         variant="permanent"
@@ -40,7 +43,7 @@ export default function SideDrawer() {
             (text, index) => (
               <ListItem key={text} disablePadding>
                 <ListItemButton>
-                  <ListItemIcon>
+                  <ListItemIcon sx={{color:"#006DFC"}}>
                     {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
                   </ListItemIcon>
                   <ListItemText primary={text} />
@@ -49,12 +52,12 @@ export default function SideDrawer() {
             )
           )}
         </List>
-        <Divider />
+        <Divider sx={{backgroundColor:"white"}}/>
         <List>
           {["All mail", "Trash", "Spam"].map((text, index) => (
             <ListItem key={text} disablePadding>
               <ListItemButton>
-                <ListItemIcon>
+                <ListItemIcon sx={{color:"#006DFC"}}>
                   {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
                 </ListItemIcon>
                 <ListItemText primary={text} />
