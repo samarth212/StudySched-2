bb2e59b575ede9cbf7c919fc258486a69298f49d
Samarth <samarth212@gmail.com>
2024-07-22 12:42:21 -0400

    change next day hours

--- src/helper/updateAssignment.js
+++ src/helper/updateAssignment.js
@@ -1,17 +1,30 @@
 export default function updateAssignment( scheduler, arrayIndex=0, dayIndex=0, hoursCompletedToday = 0 ){
 
-    const tempSchedule = [...scheduler]
+    //const tempSchedule = [...scheduler]
 
     const assignment = scheduler[arrayIndex][dayIndex]
 
-    if(scheduler && assignment.hoursSupposedtoWork <= hoursCompletedToday){
+    const nextDayAssignments = scheduler[arrayIndex+1]
+
+
+    if(scheduler && assignment.hoursSupposedtoWork === hoursCompletedToday){
         console.log("finished")
         assignment.hoursWorked = hoursCompletedToday;
         return scheduler;
     }
-    else{
+    else if(scheduler && assignment.hoursSupposedtoWork < hoursCompletedToday){
+        const hoursToSubtract = hoursCompletedToday - assignment.hoursSupposedtoWork
+        assignment.hoursWorked = hoursCompletedToday;
+        nextDayAssignments[dayIndex].hoursSupposedtoWork -= hoursToSubtract;
         return scheduler;
-    } 
+    }
+    else{
+        const hoursToAdd = assignment.hoursSupposedtoWork - hoursCompletedToday;
+        assignment.hoursWorked = hoursCompletedToday;
+        nextDayAssignments[dayIndex].hoursSupposedtoWork += hoursToAdd;
+        assignment.hoursWorked = hoursCompletedToday;
+        return scheduler;  
+    }
 
 
   
