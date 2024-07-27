11925e0c2becebbd83a7b68845708201d268014f
Samarth <samarth212@gmail.com>
2024-07-27 14:28:33 -0400

    handle last assignment

--- src/helper/updateAssignment.js
+++ src/helper/updateAssignment.js
@@ -2,7 +2,16 @@ export default function updateAssignment( scheduler, arrayIndex=0, dayIndex=0, h
 
     const assignment = scheduler[arrayIndex][dayIndex]
 
+    let isLastAssignment = false
+
     const nextDayAssignments = scheduler[arrayIndex+1]
+
+    //console.log("next", nextDayAssignments[0])
+    if (!nextDayAssignments[0]){
+        isLastAssignment = true
+    }
+
+    //console.log(isLastAssignment)
 
     let tempTotalHours = 0
 
@@ -38,54 +47,68 @@ export default function updateAssignment( scheduler, arrayIndex=0, dayIndex=0, h
         assignment.hoursWorked = hoursCompletedToday;
         //nextDayAssignments[dayIndex].hoursSupposedtoWork += hoursToAdd;
         
-        for(let i=0; i<nextDayAssignments.length; i++){
-            tempTotalHours += nextDayAssignments[i].hoursSupposedtoWork;
-        };
+        if(!isLastAssignment){
+            for(let i=0; i<nextDayAssignments.length; i++){
+                tempTotalHours += nextDayAssignments[i].hoursSupposedtoWork;
+            };
+        }
+        
         tempTotalHours += hoursToAdd
-        //console.log('hours', tempTotalHours)
         
         const newTask = JSON.parse(JSON.stringify(assignment));
         let includesAssignment = false
 
-
-        if(tempTotalHours > dailyStudyTime){
-
-            for(let i=0; i<nextDayAssignments.length; i++){
-                if(nextDayAssignments[i].name === newTask.name){
-                    includesAssignment = true;
-                    break;
-                }
-            };
-
-            if(includesAssignment){
-                const newHourstoAdd = tempTotalHours - dailyStudyTime;
-                newTask.hoursSupposedtoWork = newHourstoAdd
-                newTask.hoursWorked = 0
-                scheduler[arrayIndex+2].unshift(newTask)
-                //nextDayAssignments[dayIndex].hoursSupposedtoWork -= newHourstoAdd;
-                isBackedUp = reCheckTotalHours(scheduler[arrayIndex+2], dailyStudyTime)
-    
-                for(let i=0; i<scheduler[arrayIndex+2].length; i++){
-                    scheduler[arrayIndex+2][i].isBackedUp = isBackedUp
-                };
-            }
-            else{
-                const newHourstoAdd = tempTotalHours - dailyStudyTime;
-                newTask.hoursSupposedtoWork = newHourstoAdd
-                newTask.hoursWorked = 0
-                nextDayAssignments.unshift(newTask)
-                isBackedUp = reCheckTotalHours(nextDayAssignments, dailyStudyTime)
-    
-                for(let i=0; i<nextDayAssignments.length; i++){
-                    nextDayAssignments[i].isBackedUp = isBackedUp
-                };
-            }
-           
-
+        if(isLastAssignment){
+            newTask.hoursSupposedtoWork = hoursToAdd
+            const date = addOneDay(newTask.dateOfCompletion)
+            console.log('date', date)
+            newTask.dateOfCompletion = date
+            nextDayAssignments.unshift(newTask)
+            return scheduler
         }
         else{
-            nextDayAssignments[dayIndex].hoursSupposedtoWork += hoursToAdd;
+            if(tempTotalHours > dailyStudyTime){
+
+                for(let i=0; i<nextDayAssignments.length; i++){
+                    if(nextDayAssignments[i].name === newTask.name){
+                        includesAssignment = true;
+                        break;
+                    }
+                };
+    
+                if(includesAssignment){
+                    const newHourstoAdd = tempTotalHours - dailyStudyTime;
+                    newTask.hoursSupposedtoWork = newHourstoAdd
+                    newTask.hoursWorked = 0
+                    scheduler[arrayIndex+2].unshift(newTask)
+                    //nextDayAssignments[dayIndex].hoursSupposedtoWork -= newHourstoAdd;
+                    isBackedUp = reCheckTotalHours(scheduler[arrayIndex+2], dailyStudyTime)
+        
+                    for(let i=0; i<scheduler[arrayIndex+2].length; i++){
+                        scheduler[arrayIndex+2][i].isBackedUp = isBackedUp
+                    };
+                }
+                else{
+                    const newHourstoAdd = tempTotalHours - dailyStudyTime;
+                    newTask.hoursSupposedtoWork = newHourstoAdd
+                    newTask.hoursWorked = 0
+                    nextDayAssignments.unshift(newTask)
+                    isBackedUp = reCheckTotalHours(nextDayAssignments, dailyStudyTime)
+        
+                    for(let i=0; i<nextDayAssignments.length; i++){
+                        nextDayAssignments[i].isBackedUp = isBackedUp
+                    };
+                }
+               
+    
+            }
+            else{
+                nextDayAssignments[dayIndex].hoursSupposedtoWork += hoursToAdd;
+            }
         }
+
+
+        
     
 
         return scheduler;
@@ -112,3 +135,16 @@ function reCheckTotalHours(daySchedule, dailyStudyTime){
     };
 
 };
+
+
+function addOneDay(dateStr) {
+    const date = new Date(dateStr + 'T00:00:00'); 
+    date.setDate(date.getDate() + 1);
+
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); 
+    const day = String(date.getDate()).padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
+}
+
